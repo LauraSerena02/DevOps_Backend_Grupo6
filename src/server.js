34556f676feaca182d20152d83511d5f88f88d32
LauraSerena02 <laura.serena.rm@gmail.com>
2024-11-19 14:44:32 -0500

    fix:req.query budgetedExpense

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ app.listen(app.get('port'), () => {
 });
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
